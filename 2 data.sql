e97e8e3f4e6434e48a78e05eb66c34995b21cc39
yaakoveidelman <yeidelman@eidelmans.com>
2024-01-08 20:13:32 -0500

    initial

---
+++ 2 data.sql
@@ -0,0 +1,10 @@
+insert body(bodypart, num)
+select 'eyes', 2
+union select 'ears', 2
+union select 'nose', 1
+union select 'mouth', 1
+union select 'teeth', 32
+union select 'tongue', 1
+
+insert body(bodypart, num)
+select 'hair', 4000
