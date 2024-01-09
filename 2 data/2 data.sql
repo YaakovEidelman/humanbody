bcec3792b06b26d0e122b5e9821752edbc6b32f1
yaakoveidelman <yeidelman@eidelmans.com>
2024-01-08 23:25:27 -0500

    code review see comments in files

--- 2 data/2 data.sql
+++ 2 data/2 data.sql
@@ -1,8 +1,9 @@
+-- YE please populate sample data with planets to match your new schema
 insert body(bodypart, num)
 select 'eyes', 2
 union select 'ears', 2
 union select 'nose', 1
-union select 'moth', 1
+union select 'moth', 1 --Was this intentional?
 union select 'teeth', 32
 union select 'tongue', 1
 
